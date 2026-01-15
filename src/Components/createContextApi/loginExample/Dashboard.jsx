d288995e229fd7aff799cbdc6a01566c241ee8fa
ajaynaveen99 <ajaynaveen337@gmail.com>
2026-01-15 23:18:02 +0530

    updated chnages

--- src/Components/createContextApi/loginExample/Dashboard.jsx
+++ src/Components/createContextApi/loginExample/Dashboard.jsx
@@ -17,8 +17,8 @@ if(!user){
   )
 }
 return(<>
-<div className="Dashboard"><h1>well come to Dashboard:{user.name}</h1>
-<input type="checkbox" checked={user.isLoggedIn} />
+<div  className="flex  border items-center justify-center h-screen bg-gray-100"><h1>well come to Dashboard:{user.name}</h1>
+<input className=""type="checkbox" checked={user.isLoggedIn} />
 <button onClick={()=>setUser(null)}>Logout</button>
 </div>
 </>)

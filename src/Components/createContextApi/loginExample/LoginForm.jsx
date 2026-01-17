6590ca00aee1d797831b2a850a349f01841695ef
ajaynaveen99 <ajaynaveen337@gmail.com>
2026-01-17 10:44:35 +0530

    Refactor LoginForm component for improved styling and structure

--- src/Components/createContextApi/loginExample/LoginForm.jsx
+++ src/Components/createContextApi/loginExample/LoginForm.jsx
@@ -15,18 +15,22 @@ import useLoginContext from "../../../context/LoginContext";
     }
       
    return (
-     <div className="flex justify-center items-center h-[100vh] bg-red-200  ">
-        <form onSubmit={handleSubmit} className="flex flex-column">
+     <div className="flex justify-center items-center h-[100vh] bg-white-200   ">
+        <form onSubmit={handleSubmit} className="border border-amber-50 p-6 bg-amber-200 rounded-2xl">
+            <label>Name:</label>
             <input type="text"
             name="name"
+            className="border-2 block "
             value={userName} 
             onChange={(e)=>setUserName(()=>e.target.value)}/>
+               <label>Password:</label>
              <input type="text"
+              className="border-2 block "
             name="password"
           
             value={password} 
             onChange={(e)=>setPassword(()=>e.target.value)}/>
-            <button type="submit">Login</button>
+            <button type="submit"  className=" border-amber-20 p-1 rounded-sm block ml-20 mt-3 bg-green-200 ">Login</button>
         </form>
      </div>
    )
